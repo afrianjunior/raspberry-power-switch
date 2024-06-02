e5076f7636dd06f33e6e252dad678b99696c19e4
afrianjunior <afrian.junior26@gmail.com>
2024-06-02 22:58:51 +0700

    feat: migrate to go-rpio

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/afrianjunior/raspberry-power-off
 go 1.21
 
 require (
+	github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect
 	periph.io/x/conn/v3 v3.7.0 // indirect
 	periph.io/x/host/v3 v3.8.2 // indirect
 )
--- main.go
+++ main.go
@@ -4,60 +4,54 @@ import (
 	"fmt"
 	"time"
 
-	"periph.io/x/conn/v3/gpio"
-	"periph.io/x/host/v3"
-	"periph.io/x/host/v3/rpi"
+	"github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
-	// Initialize periph.io
-	if _, err := host.Init(); err != nil {
-		fmt.Println("Failed to initialize periph.io:", err)
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println("Failed to open gpio:", err)
 		return
 	}
+	defer rpio.Close()
 
-	// Define GPIO pins
-	touchPin := rpi.P1_11 // GPIO17
-	ledPin := rpi.P1_12   // GPIO18
+	// Pin definitions
+	touchPin := rpio.Pin(17) // GPIO17
+	ledPin := rpio.Pin(18)   // GPIO18
 
 	// Set touchPin as input
-	if err := touchPin.In(gpio.PullUp, gpio.BothEdges); err != nil {
-		fmt.Println("Failed to set touchPin as input:", err)
-		return
-	}
+	touchPin.Input()
 
 	// Set ledPin as output
-	if err := ledPin.Out(gpio.Low); err != nil {
-		fmt.Println("Failed to set ledPin as output:", err)
-		return
-	}
+	ledPin.Output()
 
-	ledState := false
+	ledState := rpio.Low // Initialize LED state to Low (off)
 
 	fmt.Println("Touch sensor is ready. Touch to toggle LED.")
 
 	for {
 		// Wait for a touch event
-		touchPin.WaitForEdge(-1)
+		if touchPin.Read() == rpio.High {
+			// Debounce: wait a short period to confirm touch
+			time.Sleep(300 * time.Millisecond)
 
-		// Debounce: wait a short period to confirm touch
-		time.Sleep(300 * time.Millisecond)
-
-		if touchPin.Read() == gpio.High {
-			// Toggle LED state
-			ledState = !ledState
-			if ledState {
-				ledPin.Out(gpio.High)
-				fmt.Println("LED is now ON")
-			} else {
-				ledPin.Out(gpio.Low)
-				fmt.Println("LED is now OFF")
+			if touchPin.Read() == rpio.High {
+				// Toggle LED state
+				if ledState == rpio.Low {
+					ledState = rpio.High
+					ledPin.High()
+					fmt.Println("LED is now ON")
+				} else {
+					ledState = rpio.Low
+					ledPin.Low()
+					fmt.Println("LED is now OFF")
+				}
 			}
-		}
 
-		// Wait for touch to end before continuing
-		for touchPin.Read() == gpio.High {
-			time.Sleep(50 * time.Millisecond)
+			// Wait for touch to end before continuing
+			for touchPin.Read() == rpio.High {
+				time.Sleep(50 * time.Millisecond)
+			}
 		}
 	}
 }
